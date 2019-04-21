6295dd47c7472db645ac7b3cc4f66f4c628ff87b
Peejay Garcia <peejgarcia@gmail.com>
2019-04-20 22:51:08 -0500

    Completed last ToDo's.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -43,8 +43,23 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        model.addAttribute("jobs", jobForm);
+        if (errors.hasErrors()) {
+            return "new-job";
 
-        return "";
+        }
 
+        Job newJob = new Job(
+                jobForm.getName(),
+                jobData.getEmployers().findById(jobForm.getEmployerId()),
+                jobData.getLocations().findById(jobForm.getLocationId()),
+                jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+                jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId())
+        );
+
+
+        jobData.add(newJob);
+
+        return "redirect:/job?id=" + newJob.getId();
     }
 }
